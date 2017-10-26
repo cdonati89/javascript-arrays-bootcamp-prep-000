6be2b627389593edda5f7cafb1e02efe3fffe332
Christopher Donati <cdonati89@gmail.com>
2017-10-26 18:22:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
-destructivelyRemoveElementFromBeginningOfArray(array) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array;
 }
